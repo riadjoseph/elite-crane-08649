8e3ad1737f49228361889b113a613c716b217281
Stackbit Code Editor <projects@stackbit.com>
2022-10-23 17:21:00 +0000

    tailwind.config.js: changed

--- tailwind.config.js
+++ tailwind.config.js
@@ -34,7 +34,7 @@ module.exports = {
             },
             fontFamily: {
                 sans: ['Montserrat', 'sans-serif'],
-                sansAlt: ['Noto Sans', 'sans-serif']
+                sansAlt: ['Rubik', 'sans-serif']
             }
         }
     },
