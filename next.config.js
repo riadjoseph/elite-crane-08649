b92274cb368c6b5793682d0cfc42adfc6da0b4c6
Stackbit Code Editor <projects@stackbit.com>
2023-01-28 20:36:27 +0000

    next.config.js: changed

--- next.config.js
+++ next.config.js
@@ -26,7 +26,17 @@ module.exports = withBundleAnalyzer({
             // enable tree shaking for development mode, on production it is on by default
           //  config.optimization.usedExports = true;
         //}
-
+    webpack: (config, { webpack, dev }) => {
+        // Tell webpack to ignore watching content files in the content folder.
+        // Otherwise webpack recompiles the app and refreshes the whole page.
+        // Instead, the src/pages/[...slug].js uses the "withRemoteDataUpdates"
+        // function to update the content on the page without refreshing the
+        // whole page
+        config.plugins.push(new webpack.WatchIgnorePlugin({ paths: [/\/content\//] }));
+        if (dev) {
+            // enable tree shaking for development mode, on production it is on by default
+            config.optimization.usedExports = true;
+        }
         return config;
     }
 });
